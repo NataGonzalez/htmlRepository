1a4cb35f640f8da4b364f3ce171f0de4901f92ef
NataliaCognizant <33319451+NataliaCognizant@users.noreply.github.com>
2017-11-03 09:25:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,5 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-// use port 3000 unless there exists a preconfigured port
-var port = process.env.port || 3000;
 
-app.listen(port);
+app.listen(process.env.PORT || 3000);
