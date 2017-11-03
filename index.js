8283d665ef8d84312c7eafbdd6ebde3b036198f1
NataliaCognizant <33319451+NataliaCognizant@users.noreply.github.com>
2017-11-03 09:36:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 
